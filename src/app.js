ca9d8cf26179ad18c4df4c966bb2af542195bb66
Austin Pivarnik <austin@Austins-MacBook-Pro.local>
2015-07-08 15:00:14 -0400

    Clean console output

--- src/app.js
+++ src/app.js
@@ -32,21 +32,6 @@ function bootstrap(container) {
 
 bootstrap(document.getElementById('app'));
 
-/** Debug -- expose appTree */
-window.appTree = appTree;
-
-/** Debug -- wrap EventEmitter */
-import { View } from 'lib/view/';
-import { wrap } from 'lib/aop/';
-wrap(View, 'prototype.emit')
-    .before(function (data) {
-        let args = Array.prototype.slice.call(data.arguments, 1);
-        let { source, sourceAction, target, targetEvent } = args[0];
-
-        if (sourceAction && (source != target)) {
-            let sourceEvent = sourceAction.split('#')[1];
-            console.log(`%c ${source} %c ${sourceEvent} %c=> %c ${target} %c ${targetEvent}`, 'background:#6E6E6E;color:#fff;', 'background:none;color:#6E6E6E;', 'color:#000;', 'background:#F58000;color:#fff;','background:none;color:rgb(20,122,0);', data);
-        } else {
-            console.log(`%c ${target} %c ${targetEvent}`, 'background:#F58000;color:#fff;', 'background:none;color:rgb(20,122,0);', data);
-        }
-    });
+/** Import debug script for dev mode */
+// Comment out when building for prod
+import './debug-mode';
