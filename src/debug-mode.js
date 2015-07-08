8cc3ebf15cb489f64f27713aca1b73cc177e6278
Austin Pivarnik <austin@Austins-MacBook-Pro.local>
2015-07-08 15:07:50 -0400

    Add missing file

---
+++ src/debug-mode.js
@@ -0,0 +1,33 @@
+/**
+ * Import this file into app.js to enable debugging
+ */
+
+import appTree from 'app/app-tree/';
+
+/** Debug -- expose appTree */
+window.appTree = appTree;
+
+/** Debug -- wrap EventEmitter */
+import { View } from 'lib/view/';
+import { wrap } from 'lib/aop/';
+
+wrap(View, 'prototype.emit')
+    .before(function (data) {
+        let args = Array.prototype.slice.call(data.arguments, 1);
+        let { source, sourceAction, target, targetEvent } = args[0];
+
+        if (sourceAction && (source != target)) {
+            let sourceEvent = sourceAction.split('#')[1];
+            console.log(`%c ${source} %c ${pad(sourceEvent, 25)} %c-> %c ${target} %c ${pad(targetEvent, 25)}`, 'background:#6E6E6E;color:#fff;', 'background:none;color:#6E6E6E;', 'color:#0097A9;', 'background:#F58000;color:#fff;','background:none;color:rgb(20,122,0);', data);
+        } else {
+            console.log(`%c ${target} %c ${pad(targetEvent, 25)}`, 'background:#F58000;color:#fff;', 'background:none;color:rgb(20,122,0);', data);
+        }
+    });
+
+function pad(str, len) {
+    if (str.length > len) {
+        return str;
+    } else {
+        return (str+"                                            ").slice(0,len);
+    }
+}
